9eb8fc7324df30c75e7bb09e5c125dc4cfb4df7c
Khubaib567 <49817481+Khubaib567@users.noreply.github.com>
2022-10-06 11:05:55 +0500

    Update user.model.ts

--- src/users/user.model.ts
+++ src/users/user.model.ts
@@ -1,1 +1,12 @@
 
+export interface UserSchema {
+    name: string;
+    email: string;
+    age: Number;
+    phoneNo:string
+}
+  
+
+export interface User extends UserSchema {
+    id: number;
+}
